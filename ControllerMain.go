09c73b9afd6906b9a84546050aa7681d88b1c1f5
DavidCurelop <dscurelop1@eafit.edu.co>
2026-09-09 20:58:47 -0500

    feat: add TerminateInstances method and update GetInstancesByTag to return instance objects for full lifecycle management

--- ControllerMain.go
+++ ControllerMain.go
@@ -34,23 +34,26 @@ func main() {
 		ec2Client: ec2Client,
 	}
 	// TODO: Step 5 - Call instanceService.LaunchInstance(...) and print the instance ID!
-	instanceID, err := instanceService.LaunchInstance(ctx, "ami-0f8a61b66d1accaee", types.InstanceTypeT2Micro, "TagInstanceTest")
+	for i := 0; i < 1; i++ {
+		instanceID, err := instanceService.LaunchInstance(ctx, "ami-0f8a61b66d1accaee", types.InstanceTypeT2Micro, "TagInstanceTest")
+
+		if err != nil {
+			log.Fatalf("failed to launch instance %s", err)
+		}
+
+		log.Printf("Launched instance: %s", instanceID)
+	}
+	instances, err := instanceService.GetInstancesByTag(ctx, "Name", "TagInstanceTest")
 
 	if err != nil {
-		log.Fatalf("failed to launch instance %s", err)
-	}
-
-	log.Printf("Launched instance: %s", instanceID)
-
-	instanceIDOutput, err := instanceService.GetInstancesByTag(ctx, "Name", "TagInstanceTest")
-
-	if err != nil{
 		log.Fatalf("Error retrieving instances %s", err)
 	}
 
-	for i, ID := range instanceIDOutput{
-		fmt.Printf("Instance %v has ID %v \n", i, ID)
+	for i, instance := range instances {
+		fmt.Printf("%v: Instance %v of type %v and has state %v\n", i, aws.ToString(instance.InstanceId), instance.InstanceType, *aws.String(string(instance.State.Name)))
 	}
+
+	instanceService.TerminateInstances(ctx, instances, -1)
 }
 
 func (s *InstanceService) LaunchInstance(ctx context.Context, imageID string, instanceType types.InstanceType, instanceTag string) (string, error) {
@@ -92,7 +95,7 @@ func (s *InstanceService) LaunchInstance(ctx context.Context, imageID string, in
 	return aws.ToString(runInstancesOutput.Instances[0].InstanceId), nil
 }
 
-func (s *InstanceService) GetInstancesByTag(ctx context.Context, tagName string, tagValue string) ([]string, error) {
+func (s *InstanceService) GetInstancesByTag(ctx context.Context, tagName string, tagValue string) ([]types.Instance, error) {
 	// TODO: Step 1 - Construct &ec2.DescribeInstancesInput with a types.Filter ("tag:" + tagName)
 	describeInstancesInput := &ec2.DescribeInstancesInput{
 		Filters: []types.Filter{{
@@ -109,13 +112,39 @@ func (s *InstanceService) GetInstancesByTag(ctx context.Context, tagName string,
 		return nil, fmt.Errorf("Something went wrong while looking for instances %s", err)
 	}
 	// TODO: Step 4 - Initialize a slice: var instanceIDs []string
-	var instanceIDs []string
+	var instances []types.Instance
 	// TODO: Step 5 - Range over Reservations and Instances, appending aws.ToString(inst.InstanceId)
 	for _, reservtion := range describeInstanceOutput.Reservations {
 		for _, instance := range reservtion.Instances {
-			instanceIDs = append(instanceIDs, *instance.InstanceId)
+			instances = append(instances, instance)
 		}
 	}
 	// TODO: Step 6 - Return instanceIDs and nil
-	return instanceIDs, nil
+	return instances, nil
+}
+
+func (s *InstanceService) TerminateInstances(ctx context.Context, instances []types.Instance, amount int ) (*ec2.TerminateInstancesOutput, error) {
+	var instancesID []string
+	if amount > len(instances) || amount < 0{
+		amount = len(instances)
+	}
+
+	for i := 0; i < amount; i++ {
+		instance := instances[i]
+		instancesID = append(instancesID, *instance.InstanceId)
+	}
+	terminateInput := &ec2.TerminateInstancesInput{
+		InstanceIds: instancesID,
+	}
+	TerminateOutput, err := s.ec2Client.TerminateInstances(ctx, terminateInput)
+
+	if err != nil {
+		return nil, fmt.Errorf("%s", err)
+	}
+
+for _, terminatedInstance := range TerminateOutput.TerminatingInstances{
+	fmt.Printf("Succesfully terminated Instance: %s Status: %s\n", *aws.String(*terminatedInstance.InstanceId), terminatedInstance.CurrentState.Name)
+	}
+	
+	return TerminateOutput, nil
 }
